89589be4f3f77629c35a176ae4b72a49de500d70
David Helkowski <github@codechild.com>
2024-04-14 20:56:11 +0900

    Remove uneeded stuffs
    Deleting all the unneeded cruft as a first step.
    Renamed darwin_pytun.c over to tuntap.c as the whole project is now MacOS specific, or at least intended to be.

--- README.md
+++
@@ -1,3 +0,0 @@
-# Linux & Darwin TUN/TAP wrapper for Python
-
-This is a fork of https://github.com/montag451/pytun with partial Darwin support.
--- pytun_pmd3/__init__.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-if sys.platform == 'win32':
-    from pytun_pmd3.wintun import TunTapDevice
-else:
-    from pytun_pmd3_c import TunTapDevice
